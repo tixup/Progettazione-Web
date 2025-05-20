7cfda2e2b8f6db619d6a77181f1e1ff4efcd6572
Matteo Tognetti <104846981+tixup@users.noreply.github.com>
2025-05-20 11:20:53 +0200

    Update index.php
    Add favicon

--- php/index.php
+++ php/index.php
@@ -27,6 +27,7 @@ $username = isset($_GET['username']) ? htmlspecialchars($_GET['username']) : '';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon">
     <title>Prenotazione Sale Studio</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/login.css">
