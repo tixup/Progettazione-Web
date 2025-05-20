3cee64d25384569108f1db70e4be4af774eca92a
Matteo Tognetti <104846981+tixup@users.noreply.github.com>
2025-05-20 11:22:51 +0200

    Update recensioni.php
    Add favicon

--- php/recensioni.php
+++ php/recensioni.php
@@ -72,6 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['voto'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon">
     <title>Recensioni - Prenotazione Sale Studio</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/home.css">
