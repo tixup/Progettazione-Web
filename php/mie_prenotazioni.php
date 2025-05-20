f198cb51b8b3f6aa07e40bb4bf5bf00ad9f6aecd
Matteo Tognetti <104846981+tixup@users.noreply.github.com>
2025-05-20 11:21:49 +0200

    Update mie_prenotazioni.php
    add favicon

--- php/mie_prenotazioni.php
+++ php/mie_prenotazioni.php
@@ -63,6 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cancella_prenotazione
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon">
     <title>Le mie prenotazioni</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/home.css">
